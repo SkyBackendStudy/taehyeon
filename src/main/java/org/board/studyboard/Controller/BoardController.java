027a4a91b197398c9a0ae2df4a2b70b377d74c47
antaehyeon <shoe1541@naver.com>
2024-04-06 16:20:00 +0900

    ci error test

--- src/main/java/org/board/studyboard/Controller/BoardController.java
+++ src/main/java/org/board/studyboard/Controller/BoardController.java
@@ -30,6 +30,7 @@ public class BoardController {
     public String list(Model model, @RequestParam(value="page", defaultValue="0") int page) {
         Page<Board> paging = boardService.getList(page);
         model.addAttribute("allBoardList", paging);
+        123
         return "list";
     }
 
